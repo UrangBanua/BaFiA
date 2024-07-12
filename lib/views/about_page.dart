1621247ac4bc6b4f030264fe0548fca48a41f5a1
afni.afdillah@live.com <UrangBanua>
2024-07-13 05:09:56 +0800

    penambahan about dan launcher icon aplikasi

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -9,8 +9,23 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About'),
       ),
-      body: const Center(
-        child: Text('About Page Content'),
+      body: ListView(
+        children: [
+          Center(
+            child: ClipOval(
+              child: Image.asset('assets/icons/logo.ico'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+                'Aplikasi Asisten Keuangan khususnya untuk Keluarga LKPD Kabupaten Hulu Sungai Tengah'),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('https://github.com/UrangBanua/BaFiA'),
+          ),
+        ],
       ),
     );
   }
--- lib/views/dashboard_page.dart
+++ lib/views/dashboard_page.dart
@@ -9,147 +9,169 @@ class DashboardPage extends StatelessWidget {
   final DashboardController dashboardController =
       Get.put(DashboardController());
 
+  DateTime? currentBackPressTime;
+
+  DashboardPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
-      drawer: DrawerMenu(),
-      body: Obx(() {
-        if (dashboardController.isLoading.value) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (dashboardController.hasError.value) {
-          return const Center(child: Text('Failed to load data'));
-        } else {
-          return RefreshIndicator(
-            onRefresh: dashboardController.fetchDashboardData,
-            child: ListView.builder(
-              itemCount: dashboardController.dashboardData.length,
-              itemBuilder: (context, index) {
-                var data = dashboardController.dashboardData[index];
-                var nilaiAnggaran = NumberFormat.currency(
-                        symbol: 'Rp ',
-                        decimalDigits: 2,
-                        locale: Localizations.localeOf(context).toString())
-                    .format(data['anggaran']);
-                var nilaiPengajuan = NumberFormat.currency(
-                        symbol: 'Rp ',
-                        decimalDigits: 2,
-                        locale: Localizations.localeOf(context).toString())
-                    .format(data['realisasi_rencana']);
-                var nilaiRealisasi = NumberFormat.currency(
-                        symbol: 'Rp ',
-                        decimalDigits: 2,
-                        locale: Localizations.localeOf(context).toString())
-                    .format(data['realisasi_rill']);
-                return Column(
-                  children: [
-                    const Text('\nSerapan Realisasi Anggaran',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    Text(
-                        '\n${DateFormat('dd MMMM y', 'id').format(DateTime.parse(data['time_update']))}',
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    Text('${data['nama_skpd']}',
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    SfRadialGauge(
-                      axes: <RadialAxis>[
-                        RadialAxis(
-                          minimum: 0,
-                          maximum: 100,
-                          canScaleToFit: true,
-                          labelOffset: 30,
-                          ranges: <GaugeRange>[
-                            GaugeRange(
-                                startValue: 0,
-                                endValue: 100,
-                                color: Colors.lightGreen,
-                                startWidth: 10,
-                                endWidth: 30),
-                            GaugeRange(
-                                label: ((data['realisasi_rill'] /
-                                            data['anggaran']) *
-                                        100)
-                                    .toStringAsFixed(2),
-                                startValue: 0,
-                                endValue: (data['realisasi_rencana'] /
-                                        data['anggaran']) *
-                                    100,
-                                color: Colors.blueAccent,
-                                startWidth: 10,
-                                endWidth: 30),
-                          ],
-                          pointers: <GaugePointer>[
-                            NeedlePointer(
-                                enableAnimation: true,
-                                animationType: AnimationType.easeOutBack,
-                                animationDuration:
-                                    3000, // add animation duration
-                                value: (data['realisasi_rill'] /
-                                        data['anggaran']) *
-                                    100),
-                          ],
-                          annotations: <GaugeAnnotation>[
-                            GaugeAnnotation(
-                                positionFactor: 0.4,
-                                angle: 90,
-                                widget: Text(
-                                    '${((data['realisasi_rill'] / data['anggaran']) * 100).toStringAsFixed(2)} %',
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold))),
-                            GaugeAnnotation(
-                              angle: 90,
-                              positionFactor: 0.7,
-                              widget: Text(
-                                'Total Anggaran\n$nilaiAnggaran',
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 14),
-                              ),
-                            ),
-                            GaugeAnnotation(
-                              angle: 90,
-                              positionFactor: 1.2,
-                              widget: Text(
-                                'Total Pengajuan Realisasi\n$nilaiPengajuan',
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 14),
-                              ),
-                            ),
-                            GaugeAnnotation(
-                              angle: 90,
-                              positionFactor: 1.7,
-                              widget: Text(
-                                'Total Pencairan Realisasi\n$nilaiRealisasi',
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 14),
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              },
-            ),
+    return WillPopScope(
+      onWillPop: () async {
+        DateTime now = DateTime.now();
+        if (currentBackPressTime == null ||
+            now.difference(currentBackPressTime!) > Duration(seconds: 2)) {
+          currentBackPressTime = now;
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text('Ketuk tombol sekali lagi untuk keluar')),
           );
+          return false;
         }
-      }),
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Dashboard')),
+        drawer: DrawerMenu(),
+        body: Obx(() {
+          if (dashboardController.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (dashboardController.hasError.value) {
+            return const Center(child: Text('Failed to load data'));
+          } else {
+            return RefreshIndicator(
+              onRefresh: dashboardController.fetchDashboardData,
+              child: ListView.builder(
+                itemCount: dashboardController.dashboardData.length,
+                itemBuilder: (context, index) {
+                  var data = dashboardController.dashboardData[index];
+                  var nilaiAnggaran = NumberFormat.currency(
+                          symbol: 'Rp ',
+                          decimalDigits: 2,
+                          locale: Localizations.localeOf(context).toString())
+                      .format(data['anggaran']);
+                  var nilaiPengajuan = NumberFormat.currency(
+                          symbol: 'Rp ',
+                          decimalDigits: 2,
+                          locale: Localizations.localeOf(context).toString())
+                      .format(data['realisasi_rencana']);
+                  var nilaiRealisasi = NumberFormat.currency(
+                          symbol: 'Rp ',
+                          decimalDigits: 2,
+                          locale: Localizations.localeOf(context).toString())
+                      .format(data['realisasi_rill']);
+                  return Column(
+                    children: [
+                      const Text('\nSerapan Realisasi Anggaran',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text(
+                          '\n${DateFormat('dd MMMM y', 'id').format(DateTime.parse(data['time_update']))}',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text('${data['nama_skpd']}',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      SfRadialGauge(
+                        axes: <RadialAxis>[
+                          RadialAxis(
+                            minimum: 0,
+                            maximum: 100,
+                            canScaleToFit: true,
+                            labelOffset: 30,
+                            ranges: <GaugeRange>[
+                              GaugeRange(
+                                  startValue: 0,
+                                  endValue: 100,
+                                  color: Colors.lightGreen,
+                                  startWidth: 10,
+                                  endWidth: 30),
+                              GaugeRange(
+                                  label: ((data['realisasi_rill'] /
+                                              data['anggaran']) *
+                                          100)
+                                      .toStringAsFixed(2),
+                                  startValue: 0,
+                                  endValue: (data['realisasi_rencana'] /
+                                          data['anggaran']) *
+                                      100,
+                                  color: Colors.blueAccent,
+                                  startWidth: 10,
+                                  endWidth: 30),
+                            ],
+                            pointers: <GaugePointer>[
+                              NeedlePointer(
+                                  enableAnimation: true,
+                                  animationType: AnimationType.easeOutBack,
+                                  animationDuration:
+                                      3000, // add animation duration
+                                  value: (data['realisasi_rill'] /
+                                          data['anggaran']) *
+                                      100),
+                            ],
+                            annotations: <GaugeAnnotation>[
+                              GaugeAnnotation(
+                                  positionFactor: 0.4,
+                                  angle: 90,
+                                  widget: Text(
+                                      '${((data['realisasi_rill'] / data['anggaran']) * 100).toStringAsFixed(2)} %',
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold))),
+                              GaugeAnnotation(
+                                angle: 90,
+                                positionFactor: 0.7,
+                                widget: Text(
+                                  'Total Anggaran\n$nilaiAnggaran',
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14),
+                                ),
+                              ),
+                              GaugeAnnotation(
+                                angle: 90,
+                                positionFactor: 1.2,
+                                widget: Text(
+                                  'Total Pengajuan Realisasi\n$nilaiPengajuan',
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14),
+                                ),
+                              ),
+                              GaugeAnnotation(
+                                angle: 90,
+                                positionFactor: 1.7,
+                                widget: Text(
+                                  'Total Pencairan Realisasi\n$nilaiRealisasi',
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                },
+              ),
+            );
+          }
+        }),
+      ),
     );
   }
 }
